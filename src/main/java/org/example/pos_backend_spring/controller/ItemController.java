d7a67084f70e2305b6e251d795f6ecf3c6e5306b
Pathum <thilinapathumrathnayaka@gmail.com>
2024-10-07 23:10:17 +0530

    Item Controller done

---
+++ src/main/java/org/example/pos_backend_spring/controller/ItemController.java
@@ -0,0 +1,85 @@
+package org.example.pos_backend_spring.controller;
+
+
+import org.example.pos_backend_spring.customerStatusCodes.SelectedErrorStatus;
+import org.example.pos_backend_spring.dto.ItemStatus;
+import org.example.pos_backend_spring.dto.impl.ItemDTO;
+import org.example.pos_backend_spring.exception.DataPersistException;
+import org.example.pos_backend_spring.exception.ItemNotFoundException;
+import org.example.pos_backend_spring.service.ItemService;
+import org.example.pos_backend_spring.util.RegexProcess;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/items")
+public class ItemController {
+
+    @Autowired
+    private ItemService itemService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveItem(@RequestBody ItemDTO itemDTO) {
+        try {
+            itemService.saveItem(itemDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }catch (DataPersistException e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @GetMapping(value = "/{itemID}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ItemStatus getSelectedNote(@PathVariable ("itemID") String itemId) {
+        if (!RegexProcess.itemIdMatcher(itemId)) {
+            return new SelectedErrorStatus(1, "Note ID is not valid");
+        }
+        return itemService.getItem(itemId);
+    }
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<ItemDTO> getALlItems(){
+        return itemService.getAllItem();
+    }
+    @DeleteMapping(value = "/{itemId}")
+    public ResponseEntity<Void> deleteItem(@PathVariable ("itemId") String itemId){
+        try {
+            if (!RegexProcess.itemIdMatcher(itemId)) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+            itemService.deleteItem(itemId);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }catch (ItemNotFoundException e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @PutMapping(value = "/{itemId}")
+    public ResponseEntity<Void> updateItem(@PathVariable ("itemId") String itemId,
+                                           @RequestBody ItemDTO updateditemDTO){
+        //validations
+        try {
+            if(!RegexProcess.itemIdMatcher(itemId) || updateditemDTO == null){
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+            itemService.updateItem(itemId,updateditemDTO);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }catch (ItemNotFoundException e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
